d16ad6662bf838fbb6a479eaec852b53d3ae14bb
Prajjwal98Dubey <prajjwaldubey003@gmail.com>
2024-01-17 00:57:41 +0530

    Added Moore Voting Algorithm

---
+++ Standard Algos/moorevoting.py
@@ -0,0 +1,43 @@
+
+# In this solution the time Complexity is O(n) but the space Cmplexity is also O(n) due to the usage of
+# dictionary so to solve this problem we have to use Moore's Voting Algorithm.
+
+
+# def majorityElement(nums):
+#     dict={}
+#     for i in nums:
+#         if(i in dict):
+#             dict[i]=dict[i]+1
+#         else:
+#             dict[i]=1
+#     for i in dict:
+#         if(dict[i]>(len(nums)//2)):
+#             return i
+#     return -1
+#
+# print(majorityElement([3,1,3,3,2]))
+
+# Moore's Voting Algoithm:-
+def majorityElement(nums):
+    count=1
+    majority_element=nums[0]
+    for i in range(1,len(nums)):
+        if(majority_element!=nums[i]):
+            count=count-1
+        else:
+            count=count+1
+        if(count==0):
+            majority_element=nums[i]
+            count=1
+    counter=0
+    for i in nums:
+        if(majority_element==i):
+            counter=counter+1
+    if(counter > len(nums)//2):
+        return majority_element
+    else:
+        return -1
+
+
+print(majorityElement([3,1,3,3,2]))
+
